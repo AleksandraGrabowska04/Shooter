5cca4c4a4d61c152e894d2ac0eaaf1e680e633e6
Vadzim Valchkovich <vol4k.w@gmail.com>
2026-01-25 16:38:22 +0100

    refactor: Simplify texture loading logic and ensure directory creation for sprite sheets and texture arrays

--- wolfenstein/texture_builder.py
+++ wolfenstein/texture_builder.py
@@ -11,11 +11,15 @@ class TextureArrayBuilder:
     def __init__(self, should_build=True):
         if should_build:
             # main textures
-            self.build(
-                load_path=BASE_DIR / 'assets' / 'textures',
-                texture_array_path=BASE_DIR / 'assets' / 'texture_array' / 'texture_array.png',
-                sprite_sheet_path=BASE_DIR / 'assets' / 'sprite_sheet' / 'sprite_sheet.png'
-            )
+            load_path = BASE_DIR / 'assets' / 'textures'
+            texture_array_path = BASE_DIR / 'assets' / 'texture_array' / 'texture_array.png'
+            sprite_sheet_path = BASE_DIR / 'assets' / 'sprite_sheet' / 'sprite_sheet.png'
+            if not texture_array_path.exists() or not sprite_sheet_path.exists():
+                self.build(
+                    load_path=load_path,
+                    texture_array_path=texture_array_path,
+                    sprite_sheet_path=sprite_sheet_path
+                )
 
     def build(self, load_path, texture_array_path, sprite_sheet_path, tex_size=TEX_SIZE):
         load_path = pathlib.Path(load_path)
@@ -25,7 +29,7 @@ class TextureArrayBuilder:
         texture_paths = [item for item in load_path.rglob('*.png') if item.is_file()]
         texture_paths = sorted(
             texture_paths,
-            key=lambda tex_path: int(re.search('\\d+', str(tex_path)).group(0))
+            key=lambda tex_path: int(re.search('\\d+', tex_path.stem).group(0))
         )
         # empty tex array
         texture_array = pg.Surface([tex_size, tex_size * len(texture_paths)], pg.SRCALPHA, 32)
@@ -40,5 +44,7 @@ class TextureArrayBuilder:
             texture_array.blit(texture, (0, i * tex_size))
             sprite_sheet.blit(texture, ((i % size) * tex_size, (i // size) * tex_size))
 
+        sprite_sheet_path.parent.mkdir(parents=True, exist_ok=True)
+        texture_array_path.parent.mkdir(parents=True, exist_ok=True)
         pg.image.save(sprite_sheet, sprite_sheet_path)
         pg.image.save(texture_array, texture_array_path)
